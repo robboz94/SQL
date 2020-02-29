ead2e707071a471212c5bef1e3f2e628f3be417d
Robboz <robertbozek94@gmail.com>
2020-02-29 12:15:59 +0100

    my 1st sql databalse - table

---
+++ book-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE books ( 
+title VARCHAR (128),
+author VARCHAR (128),
+published DATE,
+isbn VARCHAR(128),
+category VARCHAR(128),
+page_count TINYINT(4),
+publisher VARCHAR(128),
+price DECIMAL)
