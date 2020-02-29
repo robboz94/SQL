8ae92c2c44a41ec3479b57f907eaf86843532c53
Robboz <robertbozek94@gmail.com>
2020-02-29 14:34:33 +0100

    cwiczenie 4 - ograniczenia typu danych, NULL, UNIQUE wartosci domyslne - SET DEFAULT

---
+++ book-table-cw4.sql
@@ -0,0 +1,81 @@
+CREATE TABLE books ( 
+title VARCHAR (128),
+author VARCHAR (128),
+published DATE,
+isbn VARCHAR(32),
+category VARCHAR(128),
+page_count INT(255),
+publisher VARCHAR(128),
+price FLOAT);
+SHOW TABLES;
+# wyswietlenie tablicy
+DESC books;
+# zmiana zawartosci tablicy
+ALTER TABLE books ADD COLUMN title1 VARCHAR(30);
+DESC books;
+# usuwanie kolumny
+ALTER TABLE books DROP COLUMN title1;
+DESC books;
+ALTER TABLE books MODIFY COLUMN page_count INT;
+ALTER table books MODIFY COLUMN price FLOAT;
+INSERT INTO books (title,author,published,isbn,category,page_count,publisher,price)
+VALUES (
+'Spring w akcji. WydanieIV',
+'Craig Walls',
+'2018-08-13',
+'978-83-283-0849-7',
+'Programowanie java',
+624,
+'HELION',
+89.00
+);
+
+INSERT INTO books (title,author,published,isbn,category,page_count,publisher,price)
+VALUE(
+'MySQL. Vademecum profesjonalisty',
+'Paul DuBois',
+'2014-03-28',
+'978-83-246-8146-4',
+'Bazy Danych',
+1216,
+'HELION',
+149.90
+);
+
+DESC books;
+SELECT * FROM books;
+# SET SQL_SAFE_UPDATES =0
+UPDATE books SET title = 'zmiana' WHERE author = 'author';
+INSERT INTO books (
+title,
+author
+) VALUES(
+'Nowa ksiazka',
+'Autor ABC'
+);
+SELECT * FROM books;
+
+CREATE TABLE PRZYKLADOWA_TABELA (
+KOLUMNA_1 VARCHAR (10) NOT NULL,
+KOLUMNA_2 VARCHAR (10) NOT NULL
+);
+
+# CW 4
+
+ALTER TABLE books MODIFY COLUMN title VARCHAR(128) NOT NULL;
+ALTER TABLE books MODIFY COLUMN author VARCHAR(128) NOT NULL;
+ALTER TABLE books MODIFY COLUMN isbn VARCHAR(128) NOT NULL;
+ALTER TABLE books MODIFY COLUMN isbn VARCHAR(128) UNIQUE;
+
+ALTER TABLE books ALTER publisher SET DEFAULT 'nieznana';
+DESC books;
+
+ALTER TABLE books ADD COLUMN in_stock INT;
+
+DESC books;
+
+
+
+
+
+
